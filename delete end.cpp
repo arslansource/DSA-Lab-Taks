bd2341e19db1bf8f2c4c35744c75d9c7cf3a8829
Muhammad Arslan <arslanmuhammad4498@outlook.com>
2021-01-17 00:08:32 +0500

    Update delete end.cpp

--- delete end.cpp
+++ delete end.cpp
@@ -4,7 +4,6 @@ struct Node{
 	int data;
 	struct Node *next;
 };
-
 struct Node* head = NULL;
 void insert(int new_data) {
    struct Node* new_node = (struct Node*) malloc(sizeof(struct Node));
@@ -31,55 +30,11 @@ void del_end(){
 			secondlast -> next = NULL;
 		}
 		free(temp);
-		
 	}
 }
-void display() {
-   	if(head == NULL){
-   		cout<<"Empty"<<endl;
-   	}
-   	else{
-		struct Node* ptr;
-   		ptr = head;
-   		while (ptr != NULL) {
-		cout<< ptr->data <<" ";
-      	ptr = ptr->next;
-  	}
-   	cout<<endl;
-   	}
-}
 int main(){
-	int a,ch;
-	cout<<"1 for insertion"<<endl;
-	cout<<"2 for display"<<endl;
-	cout<<"3 for deletion at end"<<endl;
-	cout<<"4 to exit"<<endl;
-	do{
-		cout<<"Enter choice"<<endl;
-		cin>>ch;
-		switch(ch){
-			case 1:{
-				cout<<"Enter value"<<endl;
-				cin>>a;
-				insert(a);
-				break;
-			}
-			case 2:{
-				display();
-				break;
-			}
-			case 3:{
-				del_end();
-				break;
-			}
-			case 4:{
-				cout<<"Exitting"<<endl;
-				break;
-			}
-			default:{
-				cout<<"Wrong input"<<endl;
-				break;
-			}
-		}
-	}while(ch != 4);
+	insert(12);
+	insert(56);
+	del_end();
+	return 0;
 }
