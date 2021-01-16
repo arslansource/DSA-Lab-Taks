186118506aab781aec1435c3ff1dd1aaf072d47c
Muhammad Arslan <arslanmuhammad4498@outlook.com>
2021-01-17 01:44:59 +0500

    Update circular beginning node.cpp

--- circular beginning node.cpp
+++ circular beginning node.cpp
@@ -24,48 +24,27 @@ void printList(Node* head)
 	}
 	cout << endl;
 }
-
-// Function to detect and remove loop
-// in a linked list that may contain loop
 Node* detectAndRemoveLoop(Node* head)
 {
-	// If list is empty or has only one node
-	// without loop
 	if (head == NULL || head->next == NULL)
 		return NULL;
-
 	Node *slow = head, *fast = head;
-
-	// Move slow and fast 1 and 2 steps
-	// ahead respectively.
 	slow = slow->next;
 	fast = fast->next->next;
-
-	// Search for loop using slow and
-	// fast pointers
 	while (fast && fast->next) {
 		if (slow == fast)
 			break;
 		slow = slow->next;
 		fast = fast->next->next;
 	}
-
-	// If loop does not exist
 	if (slow != fast)
 		return NULL;
-
-	// If loop exists. Start slow from
-	// head and fast from meeting point.
-	slow = head;
 	while (slow != fast) {
 		slow = slow->next;
 		fast = fast->next;
 	}
-
 	return slow;
 }
-
-/* Driver program to test above function*/
 int main()
 {
 	Node* head = newNode(50);
@@ -73,16 +52,12 @@ int main()
 	head->next->next = newNode(15);
 	head->next->next->next = newNode(4);
 	head->next->next->next->next = newNode(10);
-
-	/* Create a loop for testing */
 	head->next->next->next->next->next = head->next->next;
-
 	Node* res = detectAndRemoveLoop(head);
 	if (res == NULL)
 		cout << "Loop does not exist";
 	else
 		cout << "Loop starting node is " << res->key;
-
 	return 0;
 }
 
