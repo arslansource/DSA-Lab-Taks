c0375c5d1d16b49b0843de31da0ec7214e935dab
Muhammad Arslan <arslanmuhammad4498@outlook.com>
2021-01-19 21:44:24 +0500

    Update remove duplicates.cpp

--- remove duplicates.cpp
+++ remove duplicates.cpp
@@ -5,21 +5,23 @@ struct Node
 	int data;
 	struct Node *next;
 };
-struct Node *newNode(int data)							// Utility function to create a new Node
-{
-Node *temp = new Node;
-temp->data = data;
-temp->next = NULL;
-return temp;
+struct Node* head = NULL;
+void insert(int value){
+	struct Node* new_node;
+	new_node = (struct Node*) malloc(sizeof(struct Node));
+	new_node->data = value;
+	new_node -> next = head;
+	head = new_node;
 }
-void removeDuplicates(struct Node *start)				/* Function to remove duplicates from a unsorted linked list */
+void removeduplicates()				/* Function to remove duplicates from a unsorted linked list */
 {
-	struct Node *ptr1, *ptr2, *dup;
-	ptr1 = start;
-	while (ptr1 != NULL && ptr1->next != NULL)			/* Pick elements one by one */
+	struct Node *ptr1 = head;
+	struct Node* ptr2 = NULL;
+	struct Node* dup = NULL;
+	while (ptr1 != NULL)			/* Pick elements one by one */
 	{
 		ptr2 = ptr1;
-		while (ptr2->next != NULL)						/* Compare the picked element with rest of the elements */
+		while (ptr2->next != NULL && ptr1->next != NULL)						/* Compare the picked element with rest of the elements */
 		{												/* If duplicate then delete it */
 			if (ptr1->data == ptr2->next->data)
 			{											/* sequence of steps is important here */
@@ -33,28 +35,48 @@ void removeDuplicates(struct Node *start)				/* Function to remove duplicates fr
 		ptr1 = ptr1->next;
 	}
 }
-void printList(struct Node *node)
-{
-	while (node != NULL)
-	{
-		printf("%d ", node->data);
-		node = node->next;
+void display(){
+	struct Node* temp;
+	temp = head;
+	while(temp != NULL){
+		cout<<temp->data<<"	";
+		temp = temp->next;
 	}
+	cout<<endl;
 }
-int main(){													/* The constructed linked list is: 10->12->11->11->12->11->10*/
-	struct Node *start = newNode(10);
-	start->next = newNode(12);
-	start->next->next = newNode(11);
-	start->next->next->next = newNode(11);
-	start->next->next->next->next = newNode(12);
-	start->next->next->next->next->next =
-									newNode(11);
-	start->next->next->next->next->next->next =
-									newNode(10);
-	printf("Linked list before removing duplicates ");
-	printList(start);
-	removeDuplicates(start);
-	printf("\nLinked list after removing duplicates ");
-	printList(start);
+int main(){
+	int value,ch;													/* The constructed linked list is: 10->12->11->11->12->11->10*/
+	cout<<"1 to insert"<<endl;
+	cout<<"2 to display"<<endl;
+	cout<<"3 to remove duplicates"<<endl;
+	cout<<"4 to exit"<<endl;
+	do{
+		cout<<"Enter choice"<<endl;
+		cin>>ch;
+		switch(ch){
+			case 1:{
+				cout<<"Enter value"<<endl;
+				cin>>value;
+				insert(value);
+				break;
+			}
+			case 2:{
+				display();
+				break;
+			}
+			case 3:{
+				removeduplicates();	
+				break;
+			}
+			case 4:{
+				cout<<"Exitting"<<endl;
+				break;
+			}
+			default:{
+				cout<<"Invalid input, Try again"<<endl;
+				break;
+			}
+		}
+	}while(ch != 4);
 	return 0;
 }
