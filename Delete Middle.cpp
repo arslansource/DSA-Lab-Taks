a27a559c2b4d899f2f1238fe576ada24524827b9
Muhammad Arslan <arslanmuhammad4498@outlook.com>
2021-01-19 22:21:59 +0500

    Update Delete Middle.cpp

--- Delete Middle.cpp
+++ Delete Middle.cpp
@@ -1,74 +1,94 @@
 #include <bits/stdc++.h>
 using namespace std;
-
-/* Link list Node */
-struct Node {
+struct Node{
 	int data;
 	struct Node* next;
 };
-// count of nodes
-int countOfNodes(struct Node* head)
-{
-	int count = 0;
-	while (head != NULL) {
-		head = head->next;
-		count++;
+struct Node* head = NULL;
+void insert(){
+	struct Node* new_node;
+	new_node = (struct Node*) malloc(sizeof(struct Node));
+	cout<<"Enter value"<<endl;
+	cin>>new_node->data;
+	new_node->next = head;
+	head = new_node;
+}
+void delete_beg(){
+	if(head == NULL){
+		cout<<"No node to delete"<<endl;
 	}
-	return count;
+	else{
+		struct Node* temp;
+		temp = head;
+		head = head -> next;
+		free(temp);
+	}
+}
+void delete_middle(){
+	if(head == NULL){
+		cout<<"LL is empty"<<endl;
+	}
+	else{
+		int pos;
+		cout<<"Enter position of node(to be deleted) from start"<<endl;
+		cin>>pos;
+		struct Node* temp = head;
+		struct Node* ptr;
+		int i = 1;
+		while(i<pos-1){
+			temp = temp->next;
+			i++;
+		}
+		ptr = temp->next;
+		temp->next = ptr->next;
+		free(ptr);
+	}
+}
+void display(){
+	struct Node* temp;
+	temp = head;
+	while(temp != NULL){
+		cout<<temp->data<<"	";
+		temp = temp->next;
+	}
+	cout<<endl;
 }
 
-// Deletes middle node and returns
-// head of the modified list
-struct Node* deleteMid(struct Node* head)
-{
-	// Base cases
-	if (head == NULL)
-		return NULL;
-	if (head->next == NULL) {
-		delete head;
-		return NULL;
-	}
-	struct Node* copyHead = head;
-
-	// Find the count of nodes
-	int count = countOfNodes(head);
-
-	// Find the middle node
-	int mid = count / 2;
-
-	// Delete the middle node
-	while (mid-- > 1) {
-		head = head->next;
-	}
-	head->next = head->next->next;
-
-	return copyHead;
-}
-void printList(struct Node* ptr)
-{
-	while (ptr != NULL) {
-		cout << ptr->data << "->";
-		ptr = ptr->next;
-	}
-	cout << "NULL\n";
-}
-Node* newNode(int data)
-{
-	struct Node* temp = new Node;
-	temp->data = data;
-	temp->next = NULL;
-	return temp;
-}
-int main()
-{
-	struct Node* head = newNode(1);
-	head->next = newNode(2);
-	head->next->next = newNode(3);
-	head->next->next->next = newNode(4);
-	cout << "Gven Linked List\n";
-	printList(head);
-	head = deleteMid(head);
-	cout << "Linked List after deletion of middle\n";
-	printList(head);
-	return 0;
+int main(){
+	int ch;
+	cout<<"! to insert"<<endl;
+	cout<<"2 to display"<<endl;
+	cout<<"3 to delete from front"<<endl;
+	cout<<"4 to delete from middle"<<endl;
+	cout<<"5 to exit"<<endl;
+	do{
+		cout<<"Enter choice"<<endl;
+		cin>>ch;
+		switch(ch){
+			case 1:{
+				insert();
+				break;
+			}
+			case 2:{
+				display();
+				break;
+			}
+			case 3:{
+				delete_beg();
+				break;
+			}
+			case 4:{
+				delete_middle();
+				break;
+			}
+			case 5:{
+				cout<<"Exitting.."<<endl;
+				break;
+			}
+			default:{
+				cout<<"Invalid input, Try again"<<endl;
+				break;
+			}
+		}
+	}while(ch != 5);
 }
